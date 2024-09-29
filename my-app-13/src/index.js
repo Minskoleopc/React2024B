e26f8e59f43c2bd9582ef680d3714f26bcea7739
unknown <Minskoleopc>
2024-09-29 18:39:43 +0530

    added

--- my-app-13/src/index.js
+++ my-app-13/src/index.js
@@ -5,6 +5,7 @@ import App2 from './App2';
 import App3 from './App3';
 import App5 from './App5';
 import App6 from './App6';
+import App7 from './App7';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +14,8 @@ root.render(
     <App2/> */}
     {/* <App3/> */}
     {/* <App5></App5> */}
-    <App6></App6>
+    {/* <App6></App6> */}
+    <App7></App7>
 
   </React.StrictMode>
 );
